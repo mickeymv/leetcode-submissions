d32bad5e719d1f5c5004f633b7651794de836639
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2016-02-16 05:53:08 +0000

    [LeetCode Sync] Runtime - 19 ms (5.56%), Memory - N/A (N/A)

---
+++ my-folder/0326-power-of-three/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n<=0) {
+            return false;
+        } else if (n==1) {
+            return true;
+        }
+        
+        while(n%3==0) {
+            n/=3;
+        }
+        
+        return n==1;
+    }
+}
