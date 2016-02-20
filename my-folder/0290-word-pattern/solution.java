c6b087e13c3719d30ac35cccbe72ffc20461fa45
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2016-02-20 22:39:04 +0000

    [LeetCode Sync] Runtime - 2 ms (12.41%), Memory - N/A (N/A)

---
+++ my-folder/0290-word-pattern/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        HashMap<String, String> patternToWord = new HashMap<String, String>();
+        HashMap<String, String> wordToPattern = new HashMap<String, String>();
+        if(pattern.length()!= words.length){
+            return false;
+        }
+        for(int i=0;i<words.length;i++){
+            if(wordToPattern.get(words[i]) != null && !wordToPattern.get(words[i]).equals(String.valueOf(pattern.charAt(i)))) {
+                return false;
+            }
+            if(patternToWord.get((String.valueOf(pattern.charAt(i))))==null){
+                patternToWord.put(String.valueOf(pattern.charAt(i)),words[i]);
+                wordToPattern.put(words[i],String.valueOf(pattern.charAt(i)));
+            } else if (!words[i].equals(patternToWord.get(String.valueOf(pattern.charAt(i))))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
