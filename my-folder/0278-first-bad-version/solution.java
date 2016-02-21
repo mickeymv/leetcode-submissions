910f1e97a41b8f4bd082e5a38ed54736a2d960d1
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2016-02-21 22:17:03 +0000

    [LeetCode Sync] Runtime - 43 ms (5.26%), Memory - N/A (N/A)

---
+++ my-folder/0278-first-bad-version/solution.java
@@ -0,0 +1,25 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int badVersion=1;
+        int early=1, late=n;
+        while(early<=late){
+            System.out.println("early= "+early+", late= "+late);
+            int m=early+((late-early)/2);
+            System.out.println("m= "+m);
+            if(isBadVersion(m)){
+                badVersion=m;
+                if(early==late){
+                    return badVersion;
+                }
+                late=m;
+            } else {
+                early=m+1;
+            }
+            System.out.println("early: "+early+", late: "+late);
+        }
+        return badVersion;
+    }
+}
