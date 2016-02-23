cef7d0fd9aa9dbb35f3b9b1f04e346264239ad39
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2016-02-23 05:26:59 +0000

    [LeetCode Sync] Runtime - 6 ms (43.52%), Memory - N/A (N/A)

---
+++ my-folder/0242-valid-anagram/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char[] charS = s.toCharArray();
+        Arrays.sort(charS);
+        String sS= new String(charS);
+        
+        char[] charT = t.toCharArray();
+        Arrays.sort(charT);
+        String sT = new String(charT);
+        
+        return sS.equals(sT);
+    }
+}
