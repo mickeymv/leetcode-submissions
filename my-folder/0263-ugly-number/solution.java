e4844ce149f80dd8fb34795b34560017d1dee659
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2016-02-23 02:01:20 +0000

    [LeetCode Sync] Runtime - 2 ms (0.95%), Memory - N/A (N/A)

---
+++ my-folder/0263-ugly-number/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean isUgly(int num) {
+        if (num<2) {
+            return num==1;
+        } else {
+            int[] ds={2,3,5};
+            
+            for(int d:ds) {
+                while(num%d==0){
+                    num/=d;
+                }
+            }
+            return num==1;
+        }
+    }
+}
