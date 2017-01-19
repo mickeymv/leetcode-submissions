bb9bf107c1f65241ea234286b8845aa712a0b52e
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-01-19 00:41:09 +0000

    [LeetCode Sync] Runtime - 14 ms (35.87%), Memory - N/A (N/A)

---
+++ my-folder/0383-ransom-note/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length() > magazine.length()){
+            return false;
+        }
+        int[] countsR = new int[26];
+        int[] countsM = new int[26];
+        for(char c:ransomNote.toCharArray()){
+            countsR[c-'a']++;
+        }
+        for(char c: magazine.toCharArray()){
+            countsM[c-'a']++;
+        }
+        for(int i=0;i<26;i++) {
+            if(countsR[i] > countsM[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
