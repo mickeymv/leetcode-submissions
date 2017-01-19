3b9b42537d01dae0e21d9ccdbebaf6c5d22e196e
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-01-19 05:17:35 +0000

    [LeetCode Sync] Runtime - 4 ms (0.77%), Memory - N/A (N/A)

---
+++ my-folder/0083-remove-duplicates-from-sorted-list/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode temp = dummy;
+        
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        while(temp.next != null) {
+            if(set.contains(temp.next.val)) {
+                temp.next = temp.next.next;
+            } else {
+                set.add(temp.next.val);
+                temp = temp.next;
+            }
+        }
+        
+        
+        
+        
+        return dummy.next;
+    }
+}
