cb3ad80279e7763a3f77321cac2e8005978ad0ce
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-01-19 00:19:10 +0000

    [LeetCode Sync] Runtime - 13 ms (5.33%), Memory - N/A (N/A)

---
+++ my-folder/0014-longest-common-prefix/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder str = new StringBuilder("");
+        if(strs.length == 0){
+            return "";
+        }
+        for(int col=0;col<strs[0].length();col++){
+            char ch = strs[0].charAt(col);
+            int row =0;
+            for( row=0;row<strs.length;row++){
+                if(col>= strs[row].length() || strs[row].charAt(col) != ch){
+                  break;  
+                } 
+            }
+            if(row<strs.length) {
+                break;
+            } else {
+                str.append(ch);
+            }
+        }
+        return str.toString();
+    }
+}
