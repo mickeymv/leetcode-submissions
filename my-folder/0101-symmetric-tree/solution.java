712b3f0f3160eaf024b82b18c64f68d49ef503ad
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-01-19 17:24:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - N/A (N/A)

---
+++ my-folder/0101-symmetric-tree/solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root,root);
+    }
+    
+    private boolean isMirror(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) return true; 
+        if (node1 == null || node2 == null) return false; 
+        
+        return (node1.val == node2.val) && isMirror(node1.left,node2.right) && isMirror(node2.left,node1.right);
+}
+    
+}
