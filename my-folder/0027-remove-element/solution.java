90325d39b33ba56087b75134271d33c3f4b7f5f8
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-01-19 04:10:42 +0000

    [LeetCode Sync] Runtime - 10 ms (0.77%), Memory - N/A (N/A)

---
+++ my-folder/0027-remove-element/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int index=0,j=0;
+        if(nums.length == 0) {
+            return 0;
+        }
+        for(j=0;j<nums.length;j++){
+            if(nums[j] == val) {
+                continue;
+            } else {
+                nums[index++]=nums[j];
+            }
+        }
+        return index;
+    }
+}
