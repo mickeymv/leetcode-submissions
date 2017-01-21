4c4c209762e02e7cc9f17e7ca1a4510e562a540d
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-01-21 23:26:56 +0000

    [LeetCode Sync] Runtime - 22 ms (29.93%), Memory - N/A (N/A)

---
+++ my-folder/0217-contains-duplicate/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums.length <= 1) {
+            return false;
+        }
+        HashSet<Integer> map = new HashSet<Integer>();
+        
+        for(int n:nums){
+        if(map.contains(n)){
+            return true;
+            
+        } else{
+            map.add(n);
+        }
+    }
+    return false;
+}
+}
