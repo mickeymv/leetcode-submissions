9918036a07664f8ef25c55b1de6412004170c55f
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-01-22 21:47:29 +0000

    [LeetCode Sync] Runtime - 1 ms (0.80%), Memory - N/A (N/A)

---
+++ my-folder/0374-guess-number-higher-or-lower/solution.java
@@ -0,0 +1,25 @@
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return myGuess(1,n);
+    }
+    
+    private int myGuess(int low, int high) {
+        if(low == high) return low; 
+        int mid = low+(high-low)/2;
+        int guess = guess(mid);
+        if(guess == 0) {
+            return mid;
+        } else if(guess<0) {
+            return myGuess(low,mid-1);
+        }
+        return myGuess(mid+1,high);
+    }
+}
