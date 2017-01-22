35ba7c9951c8a25d2241aad95a2b9bd742870989
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-01-22 20:22:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - N/A (N/A)

---
+++ my-folder/0198-house-robber/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return Math.max(nums[0],nums[1]);
+        } 
+        int[] maxLoot = new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++) {
+            maxLoot[i] = Math.max(nums[i]+maxI(i-2,nums,maxLoot),maxI(i-1,nums,maxLoot));
+        }
+        
+        return maxLoot[nums.length-1];
+    }
+    
+    private int maxI(int index, int[] nums, int[] maxLoot) {
+        if(index<0) {
+            return 0;
+        } else return maxLoot[index];
+    }
+}
