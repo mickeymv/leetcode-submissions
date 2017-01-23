78dd435bfe168a97ab46441b2fdae3eb38b43a05
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-01-23 01:54:42 +0000

    [LeetCode Sync] Runtime - 1 ms (40.97%), Memory - N/A (N/A)

---
+++ my-folder/0189-rotate-array/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        if(nums == null || nums.length <2 || k==0) {
+            return;
+        }
+        
+        int[] temp = new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++) {
+            temp[(i+k)%nums.length] = nums[i];
+        }
+        
+        System.arraycopy(temp,0,nums,0,nums.length);
+    }
+}
