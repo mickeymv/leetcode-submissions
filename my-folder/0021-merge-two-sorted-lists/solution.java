da7271deea6759f318ad177c505ef307880981c9
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-02-04 02:46:41 +0000

    [LeetCode Sync] Runtime - 15 ms (0.59%), Memory - N/A (N/A)

--- my-folder/0021-merge-two-sorted-lists/solution.java
+++ my-folder/0021-merge-two-sorted-lists/solution.java
@@ -8,33 +8,11 @@
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        
-        ListNode temp = dummy;
-        
-        while (l1 != null && l2 != null) {
-            int a = 0;
-            if(l1.val < l2.val) {
-                a= l1.val;
-                l1 = l1.next;
-            } else {
-                a= l2.val;
-                l2 = l2.next;
-            }
-            temp.next = new ListNode(a);
-            temp=temp.next;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val)  { l1.next = mergeTwoLists(l1.next,l2); return l1;}
+        else {
+            l2.next = mergeTwoLists(l1,l2.next); return l2;
         }
-        
-        ListNode rem=null;
-        
-        rem= l1 != null ? l1 : l2;
-        
-        while(rem != null) {
-            temp.next = new ListNode(rem.val);
-            rem = rem.next;
-            temp=temp.next;
-        }
-        
-        return dummy.next;
     }
 }
