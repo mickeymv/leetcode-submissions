26ded8515787a5e33236cdd976d82e9c23263506
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-02-04 20:07:04 +0000

    [LeetCode Sync] Runtime - 13 ms (5.11%), Memory - N/A (N/A)

---
+++ my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack == null || needle == null) return -1;
+
+        int startIndexOfNeedleInHaystack = -1;
+        
+        int legthOfNeedle = needle.length();
+        
+        if(needle.equals("")) return 0;
+        
+        for(int i=0;i<haystack.length();i++) {
+            if(haystack.charAt(i) == needle.charAt(0) && i+legthOfNeedle <= haystack.length()) {
+                boolean contains =  haystack.substring(i,i+legthOfNeedle).equals(needle);
+                if(contains) {
+                    return i;
+                }
+            }
+        }
+return startIndexOfNeedleInHaystack;
+        //return haystack.indexOf(needle);
+    }
+}
