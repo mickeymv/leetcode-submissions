d54536426bf2d54dc33899832f1fb5bf3cf276e6
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-02-04 14:52:29 +0000

    [LeetCode Sync] Runtime - 1 ms (9.54%), Memory - N/A (N/A)

--- my-folder/0101-symmetric-tree/solution.java
+++ my-folder/0101-symmetric-tree/solution.java
@@ -7,16 +7,43 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+ import java.util.ArrayList;
+ 
 public class Solution {
+    
+    int size=0;
+    ArrayList<Integer> list = new ArrayList<>();
+    
     public boolean isSymmetric(TreeNode root) {
-        return isMirror(root,root);
+        
+        if (root==null) return true;
+        
+        return isMirror(root.left,root.right);
     }
     
-    private boolean isMirror(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) return true; 
-        if (node1 == null || node2 == null) return false; 
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if(left == null && right == null) {
+            return true;
+        }
+        if(left==null || right==null) {
+            return false;
+        }
+        if(left.val!=right.val) return false;
         
-        return (node1.val == node2.val) && isMirror(node1.left,node2.right) && isMirror(node2.left,node1.right);
-}
+        return isMirror(left.left,right.right) && isMirror(right.left,left.right);
+    }
     
+    private void helper(TreeNode root) {
+        if (root != null) {
+            helper(root.left);
+            size++;
+            list.add(root.val);
+            helper(root.right);
+        } else {
+            size++;
+            list.add(null);
+        }
+        
+    }
 }
