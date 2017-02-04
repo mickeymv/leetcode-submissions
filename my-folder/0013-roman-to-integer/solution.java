2cac2384af2aa1826372a63946857bd9680c76f4
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-02-04 01:57:28 +0000

    [LeetCode Sync] Runtime - 87 ms (5.00%), Memory - N/A (N/A)

---
+++ my-folder/0013-roman-to-integer/solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public int romanToInt(String s) {
+        if (s == null || s.equals("")) {
+            return 0;
+        }
+        
+        int sum=0;
+        
+        int prev = 0;
+        
+        for(int i=s.length()-1;i>=0;i--) {
+            int value = 0;
+            switch(s.charAt(i)) {
+                case 'I' : value = 1;
+                break;
+                case 'V' : value = 5;
+                break;
+                case 'X' : value = 10;
+                break;
+                case 'L' : value = 50;
+                break;
+                case 'C' : value = 100;
+                break;
+                case 'D' : value = 500;
+                break;
+                case 'M' : value = 1000;
+                break;
+            }
+            if(value>=prev) {
+                sum+=value;
+            } else {
+                sum-=value;
+            }
+            prev=value;
+        }
+        
+        return sum;
+    }
+}
