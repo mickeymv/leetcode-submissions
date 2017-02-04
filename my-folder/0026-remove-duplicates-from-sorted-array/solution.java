9e7de73fab2d016315d4af80fb6d60887e7df840
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-02-04 19:55:31 +0000

    [LeetCode Sync] Runtime - 14 ms (3.71%), Memory - N/A (N/A)

--- my-folder/0026-remove-duplicates-from-sorted-array/solution.java
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,19 +1,18 @@
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        int newLength = 0, index = 0;
-        HashSet<Integer> ints = new HashSet<Integer>();
-        if (nums.length == 0) {
-            return 0;
+        if(nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return 1;
+        
+        int uniqueElements = 1;
+        
+        int lastNumber = nums[0];
+        
+        for(int i=1;i<nums.length;i++) {
+            if(nums[i]!=lastNumber) {
+                lastNumber = nums[i];
+                nums[uniqueElements++] = lastNumber;
+            } 
         }
-        for (int i =0;i<nums.length;i++) {
-            if(ints.contains(nums[i])) {
-                continue;
-            } else {
-                newLength++;
-                ints.add(nums[i]);
-                nums[index++] = nums[i];
-            }
-        }
-        return newLength;
+        return uniqueElements;
     }
 }
