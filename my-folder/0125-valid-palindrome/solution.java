09a48db9bd3c5f1d9464b5ebd92218b85fe40ef0
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-02-05 03:34:14 +0000

    [LeetCode Sync] Runtime - 8 ms (36.77%), Memory - N/A (N/A)

---
+++ my-folder/0125-valid-palindrome/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if(s==null || s.length()<2) return true;
+        
+        int i =0, j=s.length()-1;
+        
+        while(i<j) {
+            if(!Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+                continue;
+            } else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+                continue;
+            }
+            if(Character.toLowerCase(s.charAt(i++)) != Character.toLowerCase(s.charAt(j--))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
