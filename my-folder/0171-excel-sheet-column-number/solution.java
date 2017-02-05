710db6d399d9ad27316a49a1e1807489e433469b
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-02-05 17:58:35 +0000

    [LeetCode Sync] Runtime - 3 ms (2.12%), Memory - N/A (N/A)

---
+++ my-folder/0171-excel-sheet-column-number/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int titleToNumber(String s) {
+        if (s==null || s.length() < 1) return 0;
+        
+        int result=0;
+        
+        int stringCharIndex = 0;
+        
+        int iteration =0;
+
+        while(stringCharIndex<s.length()) {
+            //System.out.println("Iteration= "  +iteration);
+            char stringChar = s.charAt(stringCharIndex++);
+            //System.out.println("stringChar= " +stringChar);
+            int charValue = (int) stringChar - 'A' + 1;
+            //System.out.println("charValue= " +charValue);
+            //System.out.println("colNumber= " +colNumber);
+            //System.out.println("Math.pow(26,iteration)*charValue= " +(Math.pow(26,iteration)*charValue));
+            result = result*26+charValue;
+            //System.out.println("colNumber= " +colNumber);
+        }
+        
+        return result;
+    }
+}
