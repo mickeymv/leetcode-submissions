1929afe6bdff4c446d23b3a30c73ff09828cbc0e
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-02-07 18:28:03 +0000

    [LeetCode Sync] Runtime - 9 ms (1.06%), Memory - N/A (N/A)

---
+++ my-folder/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int a,b;
+        
+        if(p.val < q.val) {
+            a= p.val;
+            b= q.val;
+        } else {
+            a= q.val;
+            b= p.val;
+        }
+        
+        if (root.val > b) {
+            return lowestCommonAncestor(root.left,p,q);
+        } else if (root.val < a) {
+            return lowestCommonAncestor(root.right,p,q);
+        }
+        
+        return root;
+    }
+}
