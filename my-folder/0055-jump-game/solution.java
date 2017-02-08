c5d8c7cfce22a3b0530381d5c009d1628f947702
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-02-08 16:20:11 +0000

    [LeetCode Sync] Runtime - 10 ms (12.38%), Memory - N/A (N/A)

---
+++ my-folder/0055-jump-game/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        
+        if (nums.length == 1) return true;
+        
+        int maxJump = nums[0];
+
+        for(int i=1;i<nums.length && maxJump-->0;i++) {
+            if(i == nums.length-1) {
+                return true;
+            }
+            maxJump = Math.max(maxJump,nums[i]);
+        }
+    
+        return false;
+    }
+}
