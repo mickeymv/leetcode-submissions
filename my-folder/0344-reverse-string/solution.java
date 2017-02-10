e316839a0a3db5aa5d1c6454e9056ecedecb2c13
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2017-02-10 21:57:06 +0000

    [LeetCode Sync] Runtime - 2 ms (7.36%), Memory - N/A (N/A)

---
+++ my-folder/0344-reverse-string/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String reverseString(String s) {
+        if(s == null || s.length() ==1) return s;
+        
+        char[] sc = s.toCharArray();
+        
+        for(int i=0,j=s.length()-1;i<j;i++,j--) {
+            char t = sc[i];
+            sc[i] = sc[j];
+            sc[j] = t;
+        }
+        
+        return new String(sc);
+    }
+}
