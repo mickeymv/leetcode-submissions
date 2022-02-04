28db697d2cbd3df014726a322a5d2f4f6ac6a2cb
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2022-02-04 03:08:33 +0000

    [LeetCode Sync] Runtime - 85 ms (5.95%), Memory - 44.6 MB (99.92%)

--- my-folder/0001-two-sum/solution.java
+++ my-folder/0001-two-sum/solution.java
@@ -1,25 +1,17 @@
-public class Solution {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] twoIndices = new int[2];
-        if(nums.length < 3) {
-            twoIndices[0]=0;
-            twoIndices[1]=1;
-            return twoIndices;
-        }
-        
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        for(int i=0;i<nums.length;i++) {
-            int rem = target - nums[i];
-            if(map.containsKey(rem)) {
-                twoIndices[0]=map.get(rem);
-                twoIndices[1]=i;
-                return twoIndices;
-            } else {
-                map.put(nums[i],i);
+        int[] indicesToSumToTarget = new int[2];
+                
+        for (int i = 0; i< nums.length ; i++) {
+            for (int j= 0 ; j < nums.length && j != i ; j++ ) {
+                if (nums[i] + nums[j] == target) {
+                    indicesToSumToTarget[0] = i;
+                    indicesToSumToTarget[1] = j;
+                    return indicesToSumToTarget;
+                }
             }
         }
         
-        return twoIndices;
+        return indicesToSumToTarget;
     }
 }
