bd5c92b8e823c3c60a85bb9433ef43eef56bb988
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2026-07-08 19:42:51 +0000

    [LeetCode Sync] Runtime - 4 ms (38.40%), Memory - 43.4 MB (41.31%)

--- my-folder/0020-valid-parentheses/solution.java
+++ my-folder/0020-valid-parentheses/solution.java
@@ -1,43 +1,37 @@
-public class Solution {
+class Solution {
+
+    Map<Character, Character> leftToRightPs = new HashMap<>();
+    {
+        leftToRightPs.put('(', ')');
+        leftToRightPs.put('{', '}');
+        leftToRightPs.put('[', ']');
+    }
+
+
     public boolean isValid(String s) {
-        if (s==null || s.length()%2!=0) {
-            return false;
-        }
-        
-        HashMap<Character,Character> map = new HashMap<>();
-        map.put('(',')');
-        map.put('[',']');
-        map.put('{','}');
-        
-        Stack<Character> q = new Stack<>();
-        
-        for(int i=0;i<s.length();i++) {
-            switch(s.charAt(i)) {
-                case '(' :
-                    case '[' :
-                        case '{' : q.push(map.get(s.charAt(i)));
-                        break;
-                        case ')':
-                            case ']':
-                                case '}': if (!q.isEmpty() ){
-                                    System.out.println("char= "+s.charAt(i));
-                                            char popped = q.pop();
-                                            System.out.println("popped= "+popped);
-                                            if (popped != s.charAt(i)) {
-                                                System.out.println("not E");
-                                                return false;
-                                            }
-                                            } else {
-                                                return false;
-                                            }
-                                            break;
+        Stack<Character> leftPs = new Stack<>();
+
+        for(char p:s.toCharArray()) {
+            if(leftToRightPs.keySet().contains(p)) {
+                leftPs.push(p);
+            } else {
+                if (leftPs.isEmpty()) {
+                    return false;
+                }
+                Character topLeftP = leftPs.peek();
+                if(leftToRightPs.get(topLeftP)!=p) {
+                    return false;
+                } else {
+                    leftPs.pop();
+                }
             }
         }
-        
-        if(!q.isEmpty()) {
+
+        if(!leftPs.isEmpty()) {
             return false;
         }
-        
+
         return true;
     }
 }
+
