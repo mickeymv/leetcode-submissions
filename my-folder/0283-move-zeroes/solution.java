eae7d42ecd58e790c78a70bd4571f67231a6c34c
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2026-07-08 19:28:01 +0000

    [LeetCode Sync] Runtime - 2 ms (92.23%), Memory - 47.8 MB (38.11%)

--- my-folder/0283-move-zeroes/solution.java
+++ my-folder/0283-move-zeroes/solution.java
@@ -1,13 +1,27 @@
-public class Solution {
+class Solution {
     public void moveZeroes(int[] nums) {
-        int pos=0;
+        int indexOfZero = -1;
+
         for(int i=0;i<nums.length;i++) {
-            if (nums[i]!=0) {
-                nums[pos++]=nums[i];   
+            if(nums[i]==0){
+                indexOfZero=i;
+                break;
             }
         }
-        for(;pos<nums.length;pos++){
-            nums[pos]=0;
+
+        if(indexOfZero==-1) {
+            //no zeroes in the array 
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]!=0 && indexOfZero<i) {
+                nums[indexOfZero]=nums[i];
+                nums[i]=0;
+                while(nums[indexOfZero]!=0) {
+                    indexOfZero++;
+                }
+            }
         }
     }
 }
